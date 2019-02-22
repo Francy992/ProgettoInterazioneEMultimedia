4fd225d053ce193b0cea095859b1141529b72f11
Francy992 <francesco.anastasio.92@gmail.com>
2019-02-22 21:59:50 +0100

    Pulizia codice + dimensione
    Pulizia codice eliminando tutto ciò che era commentato.
    Adesso le dimensioni sono prese dinamicamente in base all'immagine di input.

--- js/first.js
+++ js/first.js
@@ -1,135 +1,8 @@
-//FUNZIONANTE PRIMA IMMAGINE****************************************
-
-    /*var myImage = new Image(512, 512);
-    myImage.src = 'source/lena.jpg';
-    document.body.appendChild(myImage);
-
-    console.log("Ciao");
-    var c = document.getElementById("imageLenaModify");
-    var ctx = c.getContext("2d");
-    var img = document.getElementById("imageLena");
-    ctx.drawImage(img, 0, 0);
-    var imgData = ctx.getImageData(0, 0, c.width, c.height);
-
-    // invert colors
-    var i;
-    for (i = 0; i < imgData.data.length; i += 4) {
-    imgData.data[i] = 255 - imgData.data[i];
-    imgData.data[i+1] = 255 - imgData.data[i+1];
-    imgData.data[i+2] = 255 - imgData.data[i+2];
-    imgData.data[i+3] = 255;
-    }
-    ctx.putImageData(imgData, 0, 0);*/
-
-// FINE FUNZIONANTE PRIMA IMMAGINE****************************************
-/*
-
-var canvas = document.getElementById("imageLenaModify2");
-var contex = canvas.getContext("2d");
-//disegno l'immagine.
-contex.drawImage(img,0,0);
-//Prendo l'immagine per lavorarci come una matrice.
-var imgMatrix = contex.getImageData(0, 0, canvas.width, canvas.height);
-for(i=0;i<imgMatrix.data.length;i+=4){
-    //Red
-    imgMatrix.data[i] = imgMatrix.data[i];
-    //Green
-    imgMatrix.data[i+1] = imgMatrix.data[i+1];
-    //Blue
-    imgMatrix.data[i+2] = imgMatrix.data[i+2];
-    //Alpha
-    //    imgMatrix.data[i+3] = 230;
-    //console.log("Imgmatrix data vale: " + imgMatrix.data[i])
-}
-*/
-
-/*var mySlider = $("#slider").slider();
-//Mostro le modifiche a schermo
-contex.putImageData(imgMatrix,0,0);
-mySlider.on("change", function(val){
-    contex = canvas.getContext("2d");
-    contex.drawImage(img,0,0);
-    imgMatrix = contex.getImageData(0, 0, canvas.width, canvas.height);
-    for(i=0;i<imgMatrix.data.length;i+=4){
-        //Red
-        if(imgMatrix.data[i]+val.value.newValue <= 255 && imgMatrix.data[i]+val.value.newValue>=0)
-            imgMatrix.data[i] = imgMatrix.data[i]+val.value.newValue;
-        else if(imgMatrix.data[i]+val.value.newValue > 255)
-            imgMatrix.data[i] = 255;
-        else 
-            imgMatrix.data[i] = 0;
-        //Green
-        if(imgMatrix.data[i+1]+val.value.newValue <= 255 && imgMatrix.data[i+1]+val.value.newValue>=0)
-            imgMatrix.data[i+1] = imgMatrix.data[i+1]+val.value.newValue;
-        else if(imgMatrix.data[i+1]+val.value.newValue > 255)
-            imgMatrix.data[i+1] = 255;
-        else 
-            imgMatrix.data[i+1] = 0;
-        //Blue
-        if(imgMatrix.data[i+2]+val.value.newValue <= 255 && imgMatrix.data[i+2]+val.value.newValue>=0)
-            imgMatrix.data[i+2] = imgMatrix.data[i+2]+val.value.newValue;
-        else if(imgMatrix.data[i+2]+val.value.newValue > 255)
-            imgMatrix.data[i+2] = 255;
-        else 
-            imgMatrix.data[i+2] = 0;
-        
-        //Alpha
-        //    imgMatrix.data[i+3] = 230;
-        //console.log("Imgmatrix data vale: " + imgMatrix.data[i]);
-    }
-    contex.putImageData(imgMatrix,0,0);
-    console.log(imgMatrix.data);
-});*/
-
-
-
 Fourier.saluta();
-//Parte 3
-/*var canvasRiga1 = document.getElementById("canvasRiga1");
-var imgRiga1 = document.getElementById("imgRiga1");
-
-var contextRiga1 = canvasRiga1.getContext("2d");
-//disegno l'immagine.
-contextRiga1.drawImage(imgRiga1,0,0);
-//Prendo l'immagine per lavorarci come una matrice.
-var imgMatrix3 = contextRiga1.getImageData(0, 0, canvasRiga1.width, canvasRiga1.height);
-for (i = 0; i < imgMatrix3.data.length; i += 4) {
-    imgMatrix3.data[i] = 255 - imgMatrix3.data[i];
-    imgMatrix3.data[i+1] = 255 - imgMatrix3.data[i+1];
-    imgMatrix3.data[i+2] = 255 - imgMatrix3.data[i+2];
-    imgMatrix3.data[i+3] = 255;
-}*/
-  /*var h_hats = [];
-  var h_es = []; // the h values
-    for (var ai = 0; ai < imgMatrix3.data.length; ai+=4) {
-        // greyscale, so you only need every 4th value
-        h_es.push(imgMatrix3.data[ai]);
-    }
-  var h =  h = function(n, m) {
-    if (arguments.length === 0) return h_es;
-
-    var idx = n*dims[0] + m;
-    return h_es[idx];}
-  Fourier.transform(h(), h_hats);
-  console.log(h_hats);
-
-  var h_primes = [];
-  var h_hats = h();
-  var dims = [256,256];
-  h_hats = Fourier.unshift(h_hats, dims);
-  Fourier.invert(h_hats, h_primes);
-*/
-
-
-
-  //h_hats = Fourier.shift(h_hats, dims);
-  //contextRiga1.putImageData(imgMatrix3  ,0,0);
-  
-
-
-//Riga 1, colori inversi:
 //Creo le variabili canvas, imagine e context
 var imgRiga1 = document.getElementById("imgRiga1");
+
+var dims = [imgRiga1.width,imgRiga1.height];
 
 var canvasRiga1 = document.getElementById("canvasRiga1");
 var contextRiga1 = canvasRiga1.getContext("2d");
@@ -144,11 +17,12 @@ var context2Riga2 = canvas2Riga2.getContext("2d");
 contextRiga1.drawImage(imgRiga1,0,0);
 context1Riga2.drawImage(imgRiga1,0,0);
 context2Riga2.drawImage(imgRiga1,0,0);
+
 //Estraggo la matrice da context e ci lavoro
 //La matrice è composta da NxNx4 pixel dove NxN sono i pixel dell'immagine e 4 sono i canali, compreso alpha
-var imgMatrix3 = contextRiga1.getImageData(0, 0, canvasRiga1.width, canvasRiga1.height);
+var imgMatrix3 = contextRiga1.getImageData(0, 0, imgRiga1.width, imgRiga1.height);
 //Operazioni sulla matrice
-coloriInvertiti(imgMatrix3);
+//coloriInvertiti(imgMatrix3);
 redLevel = getLevel(imgMatrix3, 0);
 greenLevel = getLevel(imgMatrix3, 1);
 blueLevel = getLevel(imgMatrix3, 2);
@@ -160,7 +34,7 @@ imgLogMagnitude22 = getLog(imgMatrix3);
 console.log(imgLogMagnitude22);
 //scrivo la matrice modificata nel canvas
 console.log(imgMatrixOriginal);
-  //contextRiga1.putImageData(imgMatrix3, 0, 0);
+contextRiga1.putImageData(imgMatrix3, 0, 0);
 context1Riga2.putImageData(imgLogMagnitude22, 0,0);
 //context2Riga2.putImageData(imgMatrixOriginal, 0,0);
 
@@ -168,7 +42,7 @@ context1Riga2.putImageData(imgLogMagnitude22, 0,0);
 function getLog(imgMatrix){
     var h_hats = [];
     var h_es = []; // the h values
-    var dims = [512,512];
+
     var cc = 9e-3; // contrast constant
 
 
@@ -185,7 +59,7 @@ function getLog(imgMatrix){
     }
     Fourier.transform(h(), h_hats);
     h_hats = Fourier.shift(h_hats, dims);
-    console.log(h_hats);
+    //console.log(h_hats);
 
 
 
@@ -198,7 +72,7 @@ function getLog(imgMatrix){
       }
     }
  
-    Fourier.filter(h_hats, dims, 0, 512);
+    //Fourier.filter(h_hats, dims, 0, dims[1]);
  
 
     var $h =  $h = function(k, l) {
@@ -212,7 +86,7 @@ function getLog(imgMatrix){
     //var h_hats = h();
     //h_hats = Fourier.unshift(h_hats, dims);
     //Fourier.invert(h_hats, h_primes);
-    imgMatrix2 = new ImageData(512,512);
+    imgMatrix2 = new ImageData(dims[0],dims[1]);
     var logOfMaxMag = Math.log(cc*maxMagnitude+1);
     for (var k = 0; k < dims[1]; k++) {
       for (var l = 0; l < dims[0]; l++) {
